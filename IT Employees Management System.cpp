d1441fb6818a6c38b5f02a404549dc040ef6e5e7
Veronika Zarubo <nikazarubo1@gmail.com>
2025-04-02 00:11:47 +0200

    User addition

--- IT Employees Management System.cpp
+++ IT Employees Management System.cpp
@@ -2,19 +2,108 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
+
+class Student {
+private:
+    int rollno, age;
+    string name;
+
+public:
+    Student(int stdRollno, string stdName, int stdAge) {
+        rollno = stdRollno;
+        name = stdName;
+        age = stdAge;
+    }
+    void setRollno(int stdRollno) {
+        rollno = stdRollno;
+    }
+    int getRollno() {
+        return rollno;
+    }
+    void setName(string stdName) {
+        name = stdName;
+    }
+    string getName() {
+        return name;
+    }
+    void setAge(int stdAge) {
+        age = stdAge;
+    }
+    int getAge() {
+        return age;
+    }
+    void displayStudent(){
+        cout << "Roll No : " << rollno << endl;
+        cout << "Name : " << rollno << endl;
+        cout << "Age : " << rollno << endl;
+    }
+};
+
+void addNewStudent(vector<Student>& students) {
+    int rollno, age;
+    string name;
+    cout << "Enter Rollno : " << endl;
+    cin >> rollno;
+
+    for (int i = 0;i < students.size(); i++) {
+        if (students[i].getRollno()==rollno) {
+            cout << "\t\tStudent Already Exists..." << endl;
+            return;
+        }
+    };
+
+
+    cout << "Enter name : " << endl;
+    cin >> name;
+    cout << "Enter Age" << endl;
+    cin >> age;
+
+    Student newStudent(rollno, name, age);
+    students.push_back(newStudent);
+
+    cout << "\t\tStudent Add Successfully..." << endl;
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    vector<Student> students;
+    students.push_back(Student(1, "oo", 71));
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+    char choice;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    do {
+        system("cls");
+        int op;
+        cout << "\t\t------------------------------------------" << endl;
+        cout << "\t\t\tStudent Management System" << endl;
+        cout << "\t\t------------------------------------------" << endl;
+        cout << "\t\t\t1. Add New student" << endl;
+        cout << "\t\t\t2. Display All Students" << endl;
+        cout << "\t\t\t3. Search Student" << endl;
+        cout << "\t\t\t4. Update student" << endl;
+        cout << "\t\t\t5. Delete student" << endl;
+        cout << "\t\t\t6. Exit" << endl;
+        cout << "\t\t------------------------------------------" << endl;
+        cout << "\t\tEnter Your Choice : ";
+        cin >> op;
+        switch (op) {
+        case 1:
+            addNewStudent(students);
+            break;
+        case 6:
+            exit(1);
+        default:
+            cout << "\t\tInvalid number ..." << endl;
+        }
+        cout << "\t\tDo You Want To continue [Yes / No] ? :";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
+        
+
+
+};
